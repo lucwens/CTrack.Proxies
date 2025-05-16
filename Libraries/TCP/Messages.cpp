00cf7c45ca265f2e2dfce5165007ebd30c65b4f1
luc <luc.wens@ctechmetrology.com>
2025-05-16 15:51:29 +0200

    Refactor build configurations and enhance CMessage class
    Removed `Debug|x64.Build.0` configuration from multiple projects in `CTrack5.0git.sln`, consolidating builds to use `x64`.
    
    In `Messages.cpp`, added methods for message serialization, deserialization, and error handling for JSON parsing in the `CMessage` class.
    
    Updated `Messages.h` with string constants for message types and inline functions for JSON conversion. Modified the `CMessage` constructor to require explicit initialization with ID and message type, removing the default constructor.

--- Libraries/TCP/Messages.cpp
+++ Libraries/TCP/Messages.cpp
@@ -1,1 +1,87 @@
 #include "Messages.h"
+#include "../Utility/Print.h"
+
+const std::string KeyWordID   = "ID";   // ID of the message
+const std::string KeyWordType = "Type"; // Type of the message
+
+CMessage::CMessage(const std::string &ID, const MessageType &messageType)
+{
+    m_Parameters[KeyWordID]   = ID;
+    m_Parameters[KeyWordType] = messageType;
+}
+
+CMessage::~CMessage()
+{
+}
+
+std::string CMessage::Serialize() const
+{
+    std::string serializedMessage = m_Parameters.dump();
+    return serializedMessage;
+}
+
+void CMessage::Deserialize(const std::string &data)
+{
+    try
+    {
+        m_Parameters = json::parse(data);
+    }
+    catch (const std::exception &e)
+    {
+        // Handle parsing error
+        PrintError("Error parsing message:{}", e.what());
+    };
+}
+
+const std::string &CMessage::GetID()
+{
+    if (m_Parameters.contains(KeyWordID))
+    {
+        return m_Parameters[KeyWordID].get<std::string>();
+    }
+    else
+    {
+        throw std::runtime_error("ID not found in message parameters.");
+    }
+}
+
+const void CMessage::SetID(const std::string &id)
+{
+    if (m_Parameters.contains(KeyWordID))
+    {
+        m_Parameters[KeyWordID] = id;
+    }
+    else
+    {
+        throw std::runtime_error("ID not found in message parameters.");
+    }
+}
+
+MessageType CMessage::GetType()
+{
+    if (m_Parameters.contains(KeyWordType))
+    {
+        return m_Parameters[KeyWordType].get<MessageType>();
+    }
+    else
+    {
+        throw std::runtime_error("Type not found in message parameters.");
+    }
+}
+
+void CMessage::SetType(MessageType type)
+{
+    if (m_Parameters.contains(KeyWordType))
+    {
+        m_Parameters[KeyWordType] = type;
+    }
+    else
+    {
+        throw std::runtime_error("Type not found in message parameters.");
+    }
+}
+
+json &CMessage::GetParameters()
+{
+    return m_Parameters;
+}
--- Libraries/TCP/Messages.h
+++ Libraries/TCP/Messages.h
@@ -12,19 +12,76 @@ enum class MessageType
     Warning
 };
 
-class CMessage 
+const std::string MessageTypeCommand       = "Command";
+const std::string MessageTypeCommandReturn = "CommandReturn";
+const std::string MessageTypeEvent         = "Event";
+const std::string MessageTypeWarning       = "Warning";
+
+
+
+inline void to_json(json &j, const MessageType &messageType)
+{
+    switch (messageType)
+    {
+        case MessageType::Command:
+            j = MessageTypeCommand;
+            break;
+        case MessageType::CommandReturn:
+            j = MessageTypeCommandReturn;
+            break;
+        case MessageType::Event:
+            j = MessageTypeEvent;
+            break;
+        case MessageType::Warning:
+            j = MessageTypeWarning;
+            break;
+        default:
+            j = "Unknown";
+            break; // Or throw an error
+    }
+}
+
+// 2. Convert JSON to MessageType (e.g., from a string)
+inline void from_json(const json &j, MessageType &messageType)
+{
+    if (j.is_string())
+    {
+        const std::string s = j.get<std::string>();
+        if (s == MessageTypeCommand)
+            messageType = MessageType::Command;
+        else if (s == MessageTypeCommandReturn)
+            messageType = MessageType::CommandReturn;
+        else if (s == MessageTypeEvent)
+            messageType = MessageType::Event;
+        else if (s == MessageTypeWarning)
+            messageType = MessageType::Warning;
+        else
+            throw json::type_error::create(317, "invalid string for messageType: " + s, &j); // Error for unknown string
+    }
+    else
+    {
+        throw json::type_error::create(317, "messageType must be a string in JSON", &j); // Error if not a string
+    }
+}
+
+class CMessage
 {
 
   public:
-    CMessage();
+    CMessage(const std::string &ID, const MessageType &messageType);
     virtual ~CMessage();
 
   public: // serialization
+    virtual std::string Serialize() const;
+    virtual void        Deserialize(const std::string &data);
 
   public: // accessors
+    const std::string &GetID();
+    const void         SetID(const std::string &id);
+    MessageType        GetType();
+    void               SetType(MessageType type);
+    json              &GetParameters();
 
   protected:
-    std::string m_ID;         // text containing the carried information
-    MessageType m_Type;       // type of message (Command, CommandReturn, Event, Warning)
-    json        m_Parameters; // parameters can be accessed as a JSON object
+    json m_Parameters; // parameters can be accessed as a JSON object
 };
