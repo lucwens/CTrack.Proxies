099696aa257c6509eaa38ef85f1540169cbca6d1
luc <luc.wens@ctechmetrology.com>
2025-06-17 19:09:12 +0200

    cannot show hide a console remotely properly, so needs to be done with start up parameters and tcp commands

--- Libraries/Utility/os.cpp
+++ Libraries/Utility/os.cpp
@@ -21,33 +21,41 @@ void SetConsoleVisible(bool visible)
 {
     HWND hwnd = GetConsoleWindow();
 
-    if (!hwnd && visible)
-    {
-        // Allocate a new console
-        if (AllocConsole())
-        {
-            // Redirect standard streams to the new console
-            FILE* file;
-            freopen_s(&file, "CONOUT$", "w", stdout);
-            freopen_s(&file, "CONOUT$", "w", stderr);
-            freopen_s(&file, "CONIN$", "r", stdin);
-        }
-
-        hwnd = GetConsoleWindow(); // Re-fetch after allocation
-    }
-
-    if (!hwnd)
-        return;
-
-    ShowWindow(hwnd, visible ? SW_SHOW : SW_HIDE);
-
-    // Modify extended window styles to control taskbar visibility
-    long style = GetWindowLong(hwnd, GWL_EXSTYLE);
-    SetWindowLong(hwnd, GWL_EXSTYLE, visible ? (style & ~WS_EX_TOOLWINDOW) : (style | WS_EX_TOOLWINDOW));
-
     if (visible)
     {
-        SetForegroundWindow(hwnd);
-        SetWindowPos(hwnd, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_SHOWWINDOW);
+        if (!hwnd)
+        {
+            // Allocate a new console
+            if (AllocConsole())
+            {
+                FILE *file;
+                freopen_s(&file, "CONOUT$", "w", stdout);
+                freopen_s(&file, "CONOUT$", "w", stderr);
+                freopen_s(&file, "CONIN$", "r", stdin);
+            }
+
+            hwnd = GetConsoleWindow(); // Re-fetch after allocation
+        }
+
+        if (hwnd)
+        {
+            ShowWindow(hwnd, SW_SHOW);
+            SetForegroundWindow(hwnd);
+            SetWindowPos(hwnd, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_SHOWWINDOW);
+
+            // Remove toolwindow flag to allow taskbar icon (optional)
+            long style = GetWindowLong(hwnd, GWL_EXSTYLE);
+            SetWindowLong(hwnd, GWL_EXSTYLE, style & ~WS_EX_TOOLWINDOW);
+        }
+    }
+    else
+    {
+        if (hwnd)
+        {
+            ShowWindow(hwnd, SW_HIDE);
+        }
+
+        // This will detach the console and remove its taskbar entry
+        FreeConsole();
     }
 }
